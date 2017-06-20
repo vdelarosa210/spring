b59db37429c5945226db4989a02b047420580fa3
violet <vdelarosa210@gmail.com>
2017-06-20 14:04:37 -0500

    Created a Post class. This class will ultimately represent a post record from our database. The class has private properties and getters and setters for a title and body.
    Created views for viewing all the posts and for viewing one individual post.
    
    Edited  PostsController class to return the views created above.
    
    Inside the method that shows an individual post, created a new post object and pass it to the view.
    Inside the method that shows all the posts, created a new array list and add two post objects to it, then pass that list to the view.

--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -1,38 +1,37 @@
 package com.codeup.controllers;
 
+import com.codeup.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by violet on 6/19/17.
- */
 @Controller
 public class PostsController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "posts index page";
+    public String viewAll() {
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable long id){
-        return "view an individual post";
+    public String viewIndividualPost(@PathVariable long id, Model model) {
+        // Inside the method that shows an individual post, create a new post object and pass it to the view.
+        Post post = new Post("test post", "this is my first test blog post");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
-    @GetMapping ("/posts/create")
+    @GetMapping("/posts/create")
     @ResponseBody
-    public String createForm(){
+    public String showPostForm() {
         return "view the form for creating a post";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPost(){
+    public String savePost() {
         return "create a new post";
     }
-
 }
