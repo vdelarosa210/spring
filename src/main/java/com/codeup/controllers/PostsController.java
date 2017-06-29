d39839e4d19f40c00d66f0cf2b0d145612ff8f73
violet <vdelarosa210@gmail.com>
2017-06-29 13:34:40 -0500

    Created dynamic navbar functionality Added individual ad view link

--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -40,11 +40,9 @@ public class PostsController {
 
     }
 
-    //=====add pageable=====//
     @Autowired
     private PostsRepository posts;
 
-// changed from method below to pageable //
     @GetMapping("/posts")
     public String viewAll(Model model) {
         Iterable<Post> posts = postSvc.findAll();
@@ -68,6 +66,8 @@ public class PostsController {
     public String viewIndividualPost(@PathVariable long id, Model model) {
         // Inside the method that shows an individual post, create a new post object and pass it to the view.
         Post post = postSvc.findOne(id);
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
         model.addAttribute("post", post);
         return "posts/show";
     }
@@ -110,28 +110,46 @@ public class PostsController {
     }
 
 
-//Edit Functionality//
+    //Edit Functionality//
 
     @GetMapping("/posts/{id}/edit")
     public String showEditForm(@PathVariable long id, Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (user.getId()!=id){
+            return "redirect:/posts";
+        }
         Post post = postSvc.findOne(id);
         model.addAttribute("post", post);
         return "/posts/edit";
     }
 
     @PostMapping("/posts/{id}/edit")
-    public String updatePost(@ModelAttribute Post post) {
+    public String updatePost(@ModelAttribute Post post, @RequestParam(name = "file") MultipartFile uploadedFile, Model model) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        post.setOwner(user);
+        String filename = uploadedFile.getOriginalFilename();
+        String filepath = Paths.get(uploadPath, filename).toString();
+        File destinationFile = new File(filepath);
+
+        try {
+            uploadedFile.transferTo(destinationFile);
+//            model.addAttribute("message", "File successfully uploaded!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Oops! Something went wrong! " + e);
+        }
+        post.setImageUrl(filename);
         postSvc.save(post);
         return "redirect:/posts/" + post.getId();
 
     }
 
-
+    //Delete Functionality//
 
     @PostMapping("/posts/delete")
     public String deletePost(@ModelAttribute Post post,
                              @RequestParam long id,
-                             Model model) {
+                                     Model model) {
         postSvc.deletePost(id);
         model.addAttribute("message", "Your post was deleted correctly");
         return "redirect:/posts";
